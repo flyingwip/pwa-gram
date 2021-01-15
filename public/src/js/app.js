14f51ec5f34069ff9ede6014cdecd6af0bdb99cf
Martijn Wip <martijnwip@M1.local>
2021-01-15 17:07:01 +0100

    working on push notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,7 @@
 var deferredPrompt;
+var enableNotificationButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -21,3 +24,80 @@ window.addEventListener("beforeinstallprompt", function (event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  if ("serviceWorker" in navigator) {
+    // service-worker registration is service-worker + notifications
+    navigator.serviceWorker.ready.then(function (swreg) {
+      var options = {
+        body: "From now on you will receive our updates!",
+        icon: "/src/images/icons/app-icon-96x96.png",
+        image: "/src/images/sf-boat.jpg",
+        dir: "ltr",
+        lang: "en-US", // BCP 47
+        vibrate: [100, 50, 200],
+        badge: "/src/images/icons/app-icon-96x96.png",
+        tag: "confirm-notification",
+        renotify: true,
+        actions: [
+          {
+            action: "confirm",
+            title: "Okay",
+            icon: "/src/images/icons/app-icon-96x96.png",
+          },
+          {
+            action: "cancel",
+            title: "Cancel",
+            icon: "/src/images/icons/app-icon-96x96.png",
+          },
+        ],
+      };
+      swreg.showNotification("Succesfully subscribed (from SW)!", options);
+    });
+  }
+
+  // var options = {
+  //   body: "From now on you will receive our updates!",
+  // };
+
+  // new Notification("Succesfully subscribed", options);
+}
+
+function configurePushSub() {
+  if (!("serviceWorker" in navigator)) {
+    return;
+  }
+
+  navigator.serviceWorker.ready
+    .then(function (swreg) {
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function (subscription) {
+      if (subscription === null) {
+        // create a new subscription
+      } else {
+        // We have a subscription
+      }
+    });
+}
+
+function askForNotificationPermission(event) {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted!!");
+    } else {
+      // could hide button here
+      configurePushSub();
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ("Notification" in window && "serviceWorker" in navigator) {
+  for (let i = 0; i < enableNotificationButtons.length; i++) {
+    const btn = enableNotificationButtons[i];
+    btn.style.display = "inline-block";
+    btn.addEventListener("click", askForNotificationPermission);
+  }
+}
--- public/sw.js
+++ public/sw.js
@@ -223,3 +223,21 @@ self.addEventListener("sync", function (event) {
     );
   }
 });
+
+self.addEventListener("notificationclick", function (event) {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+
+  if (action == "confirm") {
+    console.log("Confirm was chosen");
+  } else {
+    console.log(action);
+  }
+  notification.close();
+});
+
+self.addEventListener("notificationclose", function (event) {
+  console.log(event);
+});
